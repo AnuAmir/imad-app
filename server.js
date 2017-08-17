6cf03bcf55efb600a0a8289a428acc505d5253de
AnuAmir <30896832+AnuAmir@users.noreply.github.com>
2017-08-17 14:43:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -20,6 +20,10 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/ui/article1.html', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'article1.html'));
+});
+
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
